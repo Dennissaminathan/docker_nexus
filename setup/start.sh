9db85a85fa73491008cd0d8b743f37171cef50b7
Dennis Saminathan <Dennis.Saminathan@msg-global.com>
2020-04-23 12:33:55 +0530

    Script alignment

--- setup/start.sh
+++ setup/start.sh
@@ -30,27 +30,27 @@ then
     sleep 10
     sed \-e '/Jetty section/ s/^#*/#/' \-re '/# application-host=/s/^#//' \-e "/# Jetty section/a\\application-port-ssl=8443" \-i /home/appuser/app/sonatype-work/nexus3/etc/nexus.properties
 
-sed \-e "s%$jetty-http.xml%$jetty-https.xml%g" \-e '/# nexus-args=/s/^#//' \-e'/# nexus-context-path=/s/^#//' \-i /home/appuser/app/sonatype-work/nexus3/etc/nexus.properties
-##Change default key store password to custom
-sed "s%password%${NX_CERTPWD}%g" -i /home/appuser/app/nexus-$NEXUS_VERSION/etc/jetty/jetty-https.xml
+    sed \-e "s%$jetty-http.xml%$jetty-https.xml%g" \-e '/# nexus-args=/s/^#//' \-e'/# nexus-context-path=/s/^#//' \-i /home/appuser/app/sonatype-work/nexus3/etc/nexus.properties
+    ##Change default key store password to custom
+    sed "s%password%${NX_CERTPWD}%g" -i /home/appuser/app/nexus-$NEXUS_VERSION/etc/jetty/jetty-https.xml
 
-##Softlink creation for blobstore backup
-mv /home/appuser/app/sonatype-work /home/appuser/data/
-ln -s /home/appuser/data/sonatype-work  /home/appuser/app/
+    ##Softlink creation for blobstore backup
+    mv /home/appuser/app/sonatype-work /home/appuser/data/
+    ln -s /home/appuser/data/sonatype-work  /home/appuser/app/
 
 
-     echo "initialstart variable is set to $INITIALSTART"
-##Creation of certificates
+    echo "initialstart variable is set to $INITIALSTART"
+    ##Creation of certificates
     /home/appuser/app/helper/createcerts.sh
 
 
 
-echo "Create nexus certificate"
-openssl pkcs12 -export -in /home/appuser/data/certificates/cer.pem -inkey /home/appuser/data/certificates/key.pem -out /home/appuser/data/certificates/nexus.key -passout pass:${NX_CERTPWD}
+    echo "Create nexus certificate"
+    openssl pkcs12 -export -in /home/appuser/data/certificates/cer.pem -inkey /home/appuser/data/certificates/key.pem -out /home/appuser/data/certificates/nexus.key -passout pass:${NX_CERTPWD}
 
-keytool -importkeystore -noprompt -deststorepass ${NX_CERTPWD} -destkeypass ${NX_CERTPWD} -destkeystore /home/appuser/data/certificates/nexus_keystore.jks -srckeystore /home/appuser/data/certificates/nexus.key -srcstoretype PKCS12 -srcstorepass ${NX_CERTPWD}
+    keytool -importkeystore -noprompt -deststorepass ${NX_CERTPWD} -destkeypass ${NX_CERTPWD} -destkeystore /home/appuser/data/certificates/nexus_keystore.jks -srckeystore /home/appuser/data/certificates/nexus.key -srcstoretype PKCS12 -srcstorepass ${NX_CERTPWD}
 
-cp /home/appuser/data/certificates/nexus_keystore.jks /home/appuser/app/nexus-$NEXUS_VERSION/etc/ssl/keystore.jks
+    cp /home/appuser/data/certificates/nexus_keystore.jks /home/appuser/app/nexus-$NEXUS_VERSION/etc/ssl/keystore.jks
 
 fi 
 echo "Starting nexus"
